4e68efa842eaf5b811ed6c266ebae409c8b75ad8
EnriqueB938 <enbagale@gmail.com>
2026-03-30 10:01:32 +0200

    Commit

--- P2/ejercicio1.sh
+++ P2/ejercicio1.sh
@@ -10,6 +10,22 @@ fi
 
 fichero=$1
 
+#Apartado 1
+
+#Apartado 2
+
+#Apartado 3
+
+#Apartado 4
+
+#Apartado 5
+
+#Apartado 6
+
+#Apartado 7
+
+#Apartado 8
+
 #Apartado 9
 echo "Líneas con dos o más palabras con mayúsculas consecutivas:"
 cat $fichero | grep -E "^,+ [A-Z][[:alpha]]+ [A-Z][[:alpha]]+"
