9c093a35bc2dd433e668cdd69f7edc3977e39762
EnriqueB938 <enbagale@gmail.com>
2026-03-30 13:04:30 +0200

    Commit

--- P2/ejercicio4.sh
+++ P2/ejercicio4.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+fichero="/etc/passwd"
+
+#Apartado 1
+echo "1. Usuarios cuyo nombre empieza con la letra 'l': "
+cat $fichero | grep -E "^l" | sed -r 's/:.*//'
+
+#Apartado 2
+echo "2. Usuarios con shell válido: "
+cat $fichero | grep -v -E "bin/false$|/usr/bin/nologin$" | sed -r 's/:.*//'
+
+#Apartado 3
+echo "3. UID de los usuarios cuyo directorio home no está en home: "
+cat $fichero | grep -v -E 
